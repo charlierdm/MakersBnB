7f5167d12be8c0f2049357fa39204df81832ce04
Jack Gumoes <j.gumoes@gmail.com>
2021-03-02 15:03:55 +0000

    added .all and .create to Space class

--- spec/unit/space_class_spec.rb
+++ spec/unit/space_class_spec.rb
@@ -2,7 +2,26 @@ require './lib/space.rb'
 
 describe Space do
 
+  describe ".all" do
+    it 'returns a list of spaces' do
+      add_to_spaces()
+      spaces = Space.all
+      puts spaces
+      expect(spaces.first).to be_a Space
+      expect(spaces.first.name).to eq 'Schrute Farms'
+      expect(spaces.first.description).to eq 'Despite what a Mr. Gareth Keenan may have indicated in his review, Schrute Farms is a safe destination for all travelers!'
+      expect(spaces.first.price).to eq '57'
+    end
+  end
 
+  describe ".create" do
+    it 'creates a new space' do
+      result = Space.create(name: 'Schrute Farms', description: 'Despite what a Mr. Gareth Keenan may have indicated in his review, Schrute Farms is a safe destination for all travelers!', user_id: 1234, price: '57')
+      expect(result.name).to eq 'Schrute Farms'
+      expect(result.description).to eq 'Despite what a Mr. Gareth Keenan may have indicated in his review, Schrute Farms is a safe destination for all travelers!'
+      expect(result.price).to eq '57'
+    end
+  end
 
 
 end
