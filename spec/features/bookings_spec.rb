21042688021b71ecf7628181839cc74f53ee4667
charlierdm <charlie.davismiller@gmail.com>
2021-03-04 17:32:45 +0000

    added booking.find and respective test

--- spec/features/bookings_spec.rb
+++ spec/features/bookings_spec.rb
@@ -1,12 +1,12 @@
 require 'pg'
-
+# 
 # feature 'display of the bookings page' do
 #   scenario 'display a list of unavailable dates' do
 #     create_space_test
 #     visit('spaces/space')
 #   end
 # end
-#
+
 
 # fill_in "Name", with: "Test Space"
 # fill_in "Description", with: "The most beautiful test space you'll ever see"
--- spec/unit/booking_spec.rb
+++ spec/unit/booking_spec.rb
@@ -13,6 +13,34 @@ describe Booking do
     expect(booking.available).to eq("1")
   end
 
+  it 'finds all bookings for a space' do
+    test_booking = Booking.create(space_id: "200", date: "2021-03-03", user_id: "user_1", booking_status: "pending", available: "1")
+    test_booking_2 = Booking.create(space_id: "200", date: "2021-03-05", user_id: "user_1", booking_status: "pending", available: "1")
+    test_booking_3 = Booking.create(space_id: "200", date: "2021-03-06", user_id: "user_1", booking_status: "pending", available: "1")
+
+    test_booking.confirm(id: test_booking.id)
+    test_booking_2.confirm(id: test_booking_2.id)
+    booking = Booking.find_unavailable(space_id: "200")
+    p booking
+
+    #Tests that unconfirmed bookings aren't being found as unavilable.
+    expect(booking.length).to eq(2)
+    # Tests for unavailable date
+    expect(booking[0].space_id).to eq("200")
+    expect(booking[0].date).to eq("2021-03-03")
+    expect(booking[0].user_id).to eq("user_1")
+    expect(booking[0].booking_status).to eq("confirmed")
+    expect(booking[0].available).to eq("0")
+    # Tests for second unavilable date
+    expect(booking[1].space_id).to eq("200")
+    expect(booking[1].date).to eq("2021-03-05")
+    expect(booking[1].user_id).to eq("user_1")
+    expect(booking[1].booking_status).to eq("confirmed")
+    expect(booking[1].available).to eq("0")
+
+
+  end
+
    it 'confirms a booking request' #do
    #Test needs a specific booking id to work -> unsure how to do that at the moment
   #
